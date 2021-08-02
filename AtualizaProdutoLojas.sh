d03ce4f1ed4c922484aec6e8d5ef875efa6d4432
regismaicon09 <regismaicon@gmail.com>
2021-08-02 17:46:50 -0300

    update

--- AtualizaProdutoLojas.sh
+++ AtualizaProdutoLojas.sh
@@ -2,10 +2,10 @@
 
 git reset --hard
 
-git -C "/opt/pentaho/client-tools/RepositorioPentaho/AtualizaCosmos/" pull
+git -C "/opt/pentaho/client-tools/RepositorioPentaho/AtualizaProdutoLojas/" pull
 
-chmod -R 777 /opt/pentaho/client-tools/RepositorioPentaho/AtualizaCosmos/
+chmod -R 777 /opt/pentaho/client-tools/RepositorioPentaho/AtualizaProdutoLojas/
 
-/opt/pentaho/client-tools/data-integration/kitchen.sh -rep:PentahoGit -job:00_AtualizaCosmos -dir:/AtualizaCosmos -level:Basic >> /opt/pentaho/client-tools/logs/LogsAtualizaCosmos`date +%Y%m%d_%H%M%S`.txt
+/opt/pentaho/client-tools/data-integration/kitchen.sh -rep:PentahoGit -job:00_AtualizaProdutoLojas -dir:/AtualizaProdutoLojas -level:Basic >> /opt/pentaho/client-tools/logs/LogsAtualizaCosmos`date +%Y%m%d_%H%M%S`.txt
 
 mv /opt/pentaho/client-tools/logs/Logs*.txt /media/pentaho/logs/
